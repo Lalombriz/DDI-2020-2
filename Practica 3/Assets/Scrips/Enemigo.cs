19f44cba862e3db482b9d4a6098eef69e6402d27
Eduardo Gutierrez <45410761+Lalombriz@users.noreply.github.com>
2020-11-01 17:26:15 -0800

    Comit de version 1.5
    Son mis practicas hasta la del menu de items, ya que habia tenido el problema que le comente de la computadora y que no podia hacer un push de mi repo loca y esta es la nueva version.

---
+++ Practica 3/Assets/Scrips/Enemigo.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+	// Variables para gestionar el radio de vision y velocidad del enemigo
+
+	public float visionRadius;
+	public float speed;
+	
+	//Variable para guardar al jugador 
+	GameObject player;
+	
+	//Variable para guardar la posicion inicial 
+	Vector3 initialPosition;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+    // Recuperamos al jugador mediante un TAG
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+        // Guardamos la posicion incial del enemigo
+        initialPosition = transform.position;
+    }
+
+
+    void Update()
+    {
+        // por defecto nuestro objeto siempre sera nuestra poscion inicial
+        Vector3 target = initialPosition;
+        
+        //pero si la distancia hasta el jugador es menor que el radio de vision el objetivo sera el 
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist < visionRadius) target = player.transform.position;
+        
+        // finalmente movemos al enemigo en distancia a su target 
+        float fixedSpeed = speed*Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
+        
+        // debug con una linea
+        Debug.DrawLine(transform.position, target, Color.green);
+        
+    }
+    
+    void OnDrawGizmos()
+    {
+    	Gizmos.color = Color.yellow;
+    	Gizmos.DrawWireSphere(transform.position, visionRadius);
+    }
+}
