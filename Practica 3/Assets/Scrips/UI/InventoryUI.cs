bf5df36f63cd666546209d449ecbcd18213f41c4
Eduardo Gutierrez <marcelo.gutierrez@uabc.edu.mx>
2020-11-05 16:14:01 -0800

    Implementacion de Audio
    Audio e diferentes zonas del mapa

---
+++ Practica 3/Assets/Scrips/UI/InventoryUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+	public GameObject Menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+        	Menu.SetActive(!Menu.activeSelf);
+        }
+    }
+}
